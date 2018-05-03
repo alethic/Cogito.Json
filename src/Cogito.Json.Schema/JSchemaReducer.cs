21edc8bdbb84a32d42d704dc14b0242ef61df89e
Jerome Haltom <wasabi@cogito.cx>
2018-05-03 00:31:37 -0500

    Comments.

--- src/Cogito.Json.Schema/JSchemaReducer.cs
+++ src/Cogito.Json.Schema/JSchemaReducer.cs
@@ -10,7 +10,9 @@ namespace Cogito.Json.Schema.Reducers
     {
 
         /// <summary>
-        /// Produces a reduction of the given <see cref="JSchema"/>, if possible.
+        /// Produces a reduction of the given <see cref="JSchema"/>, if possible. Implementations should try to return
+        /// the <see cref="JSchema"/> instance if no changes are to be applied, but must ensure they create a clone of
+        /// the instance in case of changes.
         /// </summary>
         /// <param name="schema"></param>
         /// <returns></returns>
